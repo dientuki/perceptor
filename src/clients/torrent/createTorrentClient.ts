2a6b02e67091efab1b77df428f1ad994ac6e58d1
Juan "Dientuki" Farias <dientuki@gmail.com>
2026-03-14 00:38:17 -0300

    create torrent client

---
+++ src/clients/torrent/createTorrentClient.ts
@@ -0,0 +1,17 @@
+import { TORRENT_CLIENTS, TorrentClient } from "./types";
+import { createQbittorrentClient } from "./createQbittorrentClient";
+import { getSetting } from "@/models/settings.model";
+
+export async function createTorrentClient(): Promise<TorrentClient> {
+  const config: Record<string, string> = await getSetting(["torrent_client", "torrent_host", "torrent_port"]);
+
+  if (config.torrent_client === TORRENT_CLIENTS.QBITTORRENT) {
+    return createQbittorrentClient(config);
+  }
+
+  //if (config.torrent_client === "transmission") {
+  //  return createTransmissionStrategy(config);
+  //}
+
+  throw new Error("Unsupported torrent client");
+}
