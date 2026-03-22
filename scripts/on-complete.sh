9dcc40cc5e6ca189d244adf167103b361f608189
Juan "Dientuki" Farias <dientuki@gmail.com>
2026-03-21 22:30:31 -0300

    delete files

--- scripts/on-complete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o allexport
-[ -f ../.env ] && source ../.env
-set +o allexport
-
-curl -X POST http://${HOST:-localhost}:${PORT:-3000}/api/torrents/finished \
-  -H "Content-Type: application/json" \
-  -d "{\"infoHash\":\"$1\", \"contentPath\":\"$2\"}"
