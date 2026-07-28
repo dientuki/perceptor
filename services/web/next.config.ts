ca96efc7c3d59f44aabb2d168a359e015f831e80
Juan "Dientuki" Farias <dientuki@gmail.com>
2026-07-28 00:31:44 -0300

    add traefik

--- services/web/next.config.ts
+++ services/web/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const domain = process.env.DOMAIN || 'localhost';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [domain],
 };
 
 export default nextConfig;
