2e6bb2394f3ed1564d4c736e0887a0d610ab56da
Juan "Dientuki" Farias <dientuki@gmail.com>
2026-07-28 02:00:15 -0300

    [api] add swagger

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -3,6 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV !== 'production') {
+    const { DocumentBuilder, SwaggerModule } = await import('@nestjs/swagger');
+
+    const config = new DocumentBuilder()
+      .setTitle('Mi API REST')
+      .setDescription('Documentación interactiva')
+      .setVersion('1.0')
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+  }
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
