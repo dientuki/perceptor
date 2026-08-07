92678cd1666f4219d9bea543454a95fc88acff2c
Juan "Dientuki" Farias <dientuki@gmail.com>
2026-08-07 00:08:39 -0300

    [api] fix overview lenght

---
+++ services/api/prisma/migrations/20260807000548_widen_overview_columns/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `episodes` MODIFY `overview` TEXT NULL;
+
+-- AlterTable
+ALTER TABLE `movies` MODIFY `overview` TEXT NULL;
+
+-- AlterTable
+ALTER TABLE `shows` MODIFY `overview` TEXT NULL;
